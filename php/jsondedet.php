b77faf07a351836e37f38e10c777cc2614d13927
IGA Made Mulliarsa <dedetok@hotmail.com>
2017-11-17 15:03:16 +0700

    Update jsondedet.php

--- php/jsondedet.php
+++ php/jsondedet.php
@@ -13,6 +13,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+if (!defined("JSONDEDET")) die("Can not access directly");
 
 abstract class JSONDEDETABS {
 	// http://php.net/manual/en/function.http-response-code.php
